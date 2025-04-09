4b45df5aeaffab247412c49bac277865190df0f1
Donassan <enzo.donassan@gmail.com>
2025-04-09 10:52:39 -0300

    issoai

---
+++ JS/recuperar.js
@@ -0,0 +1,4 @@
+function atualizar() {
+    alert('Senha atualizada com sucesso!' + '\n' + 'Você está sendo redirecionado para a tela de login!')
+    setTimeout(function(){window.location.href = 'index.html';}, 1500);
+}
---
+++ JS/registro.js
@@ -0,0 +1,4 @@
+function registrar() {
+        alert('Registro realizado com sucesso!' + '\n' + 'Você está sendo redirecionado para a tela de login!')
+        setTimeout(function(){window.location.href = 'index.html';}, 1500);
+}
